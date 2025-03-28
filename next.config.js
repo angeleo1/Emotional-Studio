60442bf3b52bf2a2118df29bf912ab61a0442afa
angeleo1 <angeleo9691@gmail.com>
2025-03-28 18:09:55 +1100

    Update Next.js image optimization config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
-  }
+    domains: ['res.cloudinary.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp'],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`,
+      },
+    ],
+  },
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'en',
+  },
 }
 
 module.exports = nextConfig 
