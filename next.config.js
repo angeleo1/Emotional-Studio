4467c51d004f030eaafdd995899c98377052cd13
angeleo1 <angeleo9691@gmail.com>
2025-04-07 15:59:38 +1000

    Update build configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -13,6 +13,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig 
