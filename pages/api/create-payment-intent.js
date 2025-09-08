1fe29f536ff20bb263d72aa0b8c3ec0854b09d81
angeleo1 <angeleo9691@gmail.com>
2025-09-08 12:11:27 +1000

    Fix API to use CommonJS syntax for .js files

--- pages/api/create-payment-intent.js
+++ pages/api/create-payment-intent.js
@@ -1,10 +1,10 @@
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2023-10-16',
 });
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
