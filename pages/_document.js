04f740fbefe6a3c617bd8081e1bd4233c1d0a074
angeleo1 <angeleo9691@gmail.com>
2025-09-16 17:24:37 +1000

    fix: add comprehensive favicon support for all devices and browsers

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,37 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#ff6100" />
+        
+        {/* 파비콘 설정 - 여러 형식 지원 */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="256x256" href="/icon-512x512.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
+        
+        {/* Apple 터치 아이콘 */}
         <link rel="apple-touch-icon" sizes="180x180" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icon-192x192.png" />
+        
+        {/* Windows 타일 */}
         <meta name="msapplication-TileColor" content="#ff6100" />
         <meta name="msapplication-TileImage" content="/icon-192x192.png" />
+        <meta name="msapplication-square70x70logo" content="/icon-192x192.png" />
+        <meta name="msapplication-square150x150logo" content="/icon-192x192.png" />
+        <meta name="msapplication-wide310x150logo" content="/icon-192x192.png" />
+        <meta name="msapplication-square310x310logo" content="/icon-512x512.png" />
         {/* PWA 설치 프롬프트 방지를 위해 manifest 제거 */}
         {/* <link rel="manifest" href="/manifest.json" /> */}
         
